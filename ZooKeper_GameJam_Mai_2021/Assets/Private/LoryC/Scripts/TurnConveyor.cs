fda5ec6ccf010d8819871ce5e8d5ef260b08bce1
KyanosAlpha <66301729+KyanosAlpha@users.noreply.github.com>
2021-05-10 16:53:40 +0200

    Script TurnCage + Prefabs Cage PlaceOlder

---
+++ ZooKeper_GameJam_Mai_2021/Assets/Private/LoryC/Scripts/TurnConveyor.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class TurnConveyor : MonoBehaviour
+{
+    #region Public Members
+
+    #endregion
+
+
+    #region Unity API
+
+    #endregion
+
+
+    #region Private And Protected
+
+    #endregion
+}
