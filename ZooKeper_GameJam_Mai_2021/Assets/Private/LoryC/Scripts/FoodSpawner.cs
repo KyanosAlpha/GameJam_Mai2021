ee742ca228f29e01a5752a2471489a9bc45305f3
KyanosAlpha <66301729+KyanosAlpha@users.noreply.github.com>
2021-05-11 14:06:57 +0200

    Fixed Script SpawnerFood + features

--- ZooKeper_GameJam_Mai_2021/Assets/Private/LoryC/Scripts/FoodSpawner.cs
+++ ZooKeper_GameJam_Mai_2021/Assets/Private/LoryC/Scripts/FoodSpawner.cs
@@ -9,25 +9,45 @@ public class FoodSpawner : MonoBehaviour
     {
         _transform = transform;
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(_transform.position, _transform.forward);
+        Gizmos.DrawLine(_transform.position, _transform.position + _transform.forward * 1);
     }
 
     private void Awake()
     {
-        _transform = transform;
-        _spawnTime = Time.time + _nextSpawnTime;
+        InitializeOnAwake();
     }
 
     private void Update()
     {
-        if(Time.time > _spawnTime)
-        {
-            int prefabIndex = Random.Range(0, _foodsList.Count);
-            Instantiate(_foodsList[prefabIndex], _transform.position, _transform.rotation);
+        SpawningFoods();
+    }
 
-            _spawnTime += _nextSpawnTime;
+    #endregion
+
+    #region Utils
+
+    private void InitializeOnAwake()
+    {
+        _transform = transform;
+        _spawnedFoods = new List<Transform>();
+        _spawnTime = Time.time + _nextSpawnTimeMax;
+    }
+
+    private void SpawningFoods()
+    {
+        if (_spawnedFoods.Count < _maxOfSpawn)
+        {
+            if (Time.time > _spawnTime)
+            {
+                var waintingToSpawn = Random.Range(_nextSpawnTimeMin, _nextSpawnTimeMax);
+                var prefabIndex = Random.Range(0, _foodsList.Count);
+
+                Instantiate(_foodsList[prefabIndex], _transform.position, _transform.rotation);
+                _spawnedFoods.Add(_foodsList[prefabIndex]);
+
+                _spawnTime += waintingToSpawn;
+            }
         }
-        
     }
 
     #endregion
@@ -36,13 +56,17 @@ public class FoodSpawner : MonoBehaviour
     #region Private And Protected
 
     private Transform _transform;
-
     [SerializeField]
     private List<Transform> _foodsList;
+    private List<Transform> _spawnedFoods;
 
     private float _spawnTime;
     [SerializeField]
-    private float _nextSpawnTime = 2f;
+    private float _nextSpawnTimeMin = 0.1f;
+    [SerializeField]
+    private float _nextSpawnTimeMax = 1f;
+    [SerializeField]
+    private int _maxOfSpawn = 5;
 
     #endregion
 }
