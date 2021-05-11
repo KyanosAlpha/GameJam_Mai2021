ab984209d9e5ca554e6a005d736e21ee790a626b
Breniak <kyllian_bocklandt@hotmail.com>
2021-05-11 17:26:19 +0200

    add conveyor animation

--- ZooKeper_GameJam_Mai_2021/Assets/Private/Kyllian/MyScripts/Conveyor.cs
+++ ZooKeper_GameJam_Mai_2021/Assets/Private/Kyllian/MyScripts/Conveyor.cs
@@ -5,6 +5,16 @@ public class Conveyor : MonoBehaviour
 {
     #region Unity API
 
+    private void Awake()
+    {
+        if(_renderer == null) { _renderer = GetComponent<MeshRenderer>(); }
+    }
+
+    private void Update()
+    {
+        BeltAnimation();
+    }
+
     private void OnTriggerStay(Collider other)
     {
         if(other.CompareTag("Meat") || other.CompareTag("Vegetable"))
@@ -18,10 +28,23 @@ public class Conveyor : MonoBehaviour
     #endregion
 
 
+    #region Utils
+
+    private void BeltAnimation()
+    {
+        float offset = Time.time * _speed * -0.48f;
+
+        _renderer.material.SetTextureOffset("_MainTex", new Vector2(0f, offset));
+    }
+
+    #endregion
+
+
     #region Private And Protected Members
 
     [SerializeField] private Transform _endPoint;
     [SerializeField] private float _speed = 2f;
+    [SerializeField] private MeshRenderer _renderer;
 
     #endregion
 }
